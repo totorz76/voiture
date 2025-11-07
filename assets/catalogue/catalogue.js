c9642e7c5071c1d4603b2dff19bf180ca47693c4
totorz76 <victor76.zammit@gmail.com>
2025-11-07 18:47:52 +0100

    media query

--- assets/catalogue/catalogue.js
+++ assets/catalogue/catalogue.js
@@ -4,48 +4,55 @@ export const cars =[
         description : "Diesel, 5 portes, GPS, Autoradio, Forfait 1000km (0,5/km supplémentaire)",
         prix : "999€",
         vendeur : "Agence de Paris",
-        image : "vehicule1.png"
+        image : ["vehicule1.png", "voiture1bis.jpeg"],
+        // imagebis : "voiture1bis.jpeg"
     },
     {
         nom : "Ford Focus",
         description : "Diesel, 5 portes, GPS, Autoradio, Forfait 1000km (0,5/km supplémentaire)",
         prix : "999€",
         vendeur : "Agence de Paris",
-        image : "vehicule2.png"
+        image : ["vehicule2.png", "voiture2bis.jpg"],
+        // imagebis : "voiture2bis.jpg"
     },
     {
         nom : "Voiture 3",
         description : "Diesel, 5 portes, GPS, Autoradio, Forfait 1000km (0,5/km supplémentaire)",
         prix : "999€",
         vendeur : "Agence de Paris",
-        image : "voiture3.jpg"
+        image : ["voiture3.jpg", "voiture3bis.jpg"],
+        // imagebis : "voiture3bis.jpg"
     },
     {
         nom : "Opel Mokka",
         description : "Diesel, 5 portes, GPS, Autoradio, Forfait 1000km (0,4/km supplémentaire)",
         prix : "1150€",
         vendeur : "Agence de Paris",
-        image : "vehicule4.png"
+        image : ["vehicule4.png", "voiture4bis.webp"],  
+        // imagebis : "voiture4bis.webp"
     },
     {
         nom : "Voiture 5",
         description : "Diesel, 5 portes, GPS, Autoradio, Forfait 1000km (0,5/km supplémentaire)",
         prix : "986€",
         vendeur : "Agence de Paris",
-        image : "voiture5.webp"
+        image : ["voiture5.webp", "voiture5bis.webp"]
+        // imagebis : "voiture5bis.webp"
     },
     {
         nom : "Voiture 6",
         description : "Diesel, 5 portes, GPS, Autoradio, Forfait 1000km (0,5/km supplémentaire)",
         prix : "1200€",
         vendeur : "Agence de Paris",
-        image : "voiture6.jpg"
+        image : ["voiture6.jpg", "voiture6bis.jpg"],
+        // imagebis : "voiture6bis.jpg"
     },
     {
         nom : "Voiture 7",
         description : "Diesel, 5 portes, GPS, Autoradio, Forfait 1000km (0,5/km supplémentaire)",
         prix : "850€",
         vendeur : "Agence de Paris",
-        image : "vehicule1.png"
+        image : ["voiture7.jpg", "voiture7bis.webp"],
+        // imagebis : "voiture7bis.webp"
     },
 ]
--- assets/js/lib/card.js
+++ assets/js/lib/card.js
@@ -3,25 +3,44 @@ import { cars } from "../../catalogue/catalogue.js"
 let result = document.getElementById("result");
 const articles = document.getElementById("articles");
 
-cars.forEach(car => {
+cars.forEach((car) => {
     result.textContent = `${cars.length} résultats`;
     const article = document.createElement("article");
     article.classList.add("card");
-    article.innerHTML = 
-    `<div>
-        <i class="fa-solid fa-caret-left"></i>
-        <img src="./assets/img/${car.image}" alt="${car.nom}">
-        <i class="fa-solid fa-caret-right"></i>
+
+    let index = 0;
+
+    article.innerHTML = `
+    <div class="slider">
+      <i class="fa-solid fa-caret-left"></i>
+      <img src="./assets/img/${car.image[index]}" alt="${car.nom}">
+      <i class="fa-solid fa-caret-right"></i>
     </div>
     <div>
-        <h2>${car.nom}</h2> 
-        <p>${car.description}</p>
-        <p>${car.prix}</p>
-        <p>${car.vendeur}</p>
-        <button>Reserver et Payer</button>
-    <div>`;     
+      <h2>${car.nom}</h2> 
+      <p>${car.description}</p>
+      <p>${car.prix}</p>
+      <p>${car.vendeur}</p>
+      <button>Réserver et Payer</button>
+    </div>
+  `;
     articles.append(article);
+
+    const img = article.querySelector("img");
+    const next = article.querySelector(".fa-caret-right");
+    const prev = article.querySelector(".fa-caret-left");
+
+    next.addEventListener("click", () => {
+        index = (index + 1) % car.image.length;
+        img.src = `./assets/img/${car.image[index]}`;
+    });
+
+    prev.addEventListener("click", () => {
+        index = (index - 1 + car.image.length) % car.image.length;
+        img.src = `./assets/img/${car.image[index]}`;
+    });
 });
+
 
 
 export { articles }
